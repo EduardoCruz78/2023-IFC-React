5f5ed13bd34da01b82d6bdc9e0accc5f871d70c5
Eduardo Proença da Cruz <106383726+EduardoCruz78@users.noreply.github.com>
2023-10-18 20:05:46 +0000

    commit chat

---
+++ projetinhoblaze/src/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css"
+
+export default ()=>{
+  return <>
+    <main> 
+      <div className="message">
+        <div className="user">
+          Dunha, aquele!
+        </div>
+        <div className="text">
+          aaaaaaaaaaasdgasksafam,kcokadokfdlpaewolflaopdspofasfopas
+        </div>
+      </div>
+
+      <div className="message self">
+        <div className="user">
+          Dunha, aquele!
+        </div>
+        <div className="text">
+          aaaaaaaaaaasdgasksafam,kcokadokfdlpaewolflaopdspofasfopasssssssssssss
+        </div>
+      </div>
+
+      
+
+    </main>
+
+    <div className="input-area">
+        <input type="text" placeholder="Digite sua mensagem" />
+        <button>Enviar</button>
+      </div>
+  </>
+}
+
